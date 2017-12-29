7df6b770e7933b160174a3788ad2fb0b01a8862d
jasongurwin <>
2017-12-29 14:37:59 -0800

    working scaffolding

--- server.js
+++ server.js
@@ -1,24 +1,47 @@
-const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
 const cors = require('cors');
+const errorhandler = require('errorhandler');
+const express = require('express');
 
-const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+const apiRouter = require('./api/api.js');
 
+const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(express.static('public'));
-app.use(morgan('dev'));
+// app.use(express.static('public'));
+
 app.use(bodyParser.json());
 app.use(cors());
 
-const apiRouter = require('./api/api.js')
-apiRouter.use('/api',apiRouter);
+app.use('/api', apiRouter);
+
+app.use(errorhandler());
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
 
 module.exports = app;
+
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const errorhandler = require('errorhandler');
+// const express = require('express');
+//
+// const apiRouter = require('./api/api');
+//
+// const app = express();
+// const PORT = process.env.PORT || 4000;
+// //
+// app.use(bodyParser.json());
+// app.use(cors());
+//
+// app.use('/api', apiRouter);
+//
+// app.use(errorhandler());
+//
+// app.listen(PORT, () => {
+//   console.log('Listening on port: ' + PORT);
+// });
+//
+// module.exports = app;
